76a9377c1af01a2fcff9571cc99303a7f8cfa3a0
cutsea110 <cutsea110@gmail.com>
2021-07-10 03:07:51 +0900

    decode json and encode Problem

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,9 +7,74 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Lib
     ( someFunc
     ) where
+
+import Control.Applicative
+import Data.Aeson
+
+data Point = Point { x :: Int
+                   , y :: Int
+                   } deriving (Show, Eq)
+
+instance FromJSON Point where
+  parseJSON jsn = do
+    [x, y] <- parseJSON jsn
+    return $ Point x y
+
+instance ToJSON Point where
+  toJSON (Point x y) = toJSON [x, y]
+
+type Index = Int
+
+data Edge = Edge { s :: Index -- ^ start
+                 , e :: Index -- ^ end
+                 } deriving (Show, Eq)
+
+instance FromJSON Edge where
+  parseJSON jsn = do
+    [x, y] <- parseJSON jsn
+    return $ Edge x y
+
+instance ToJSON Edge where
+  toJSON (Edge x y) = toJSON [x, y]
+
+type Hole = [Point]
+type Vertices = [Point]
+type Edges = [Edge]
+
+data Figure = Figure { edges :: [Point]
+                     , verticecs :: [Point]
+                     } deriving Show
+
+instance FromJSON Figure where
+  parseJSON = withObject "figure" $ \o -> do
+    edges    <- o .: "edges"
+    vertices <- o .: "vertices"
+    return $ Figure edges vertices
+
+instance ToJSON Figure where
+  toJSON (Figure es vs)
+    = object [ "edges" .= es, "vertices" .= vs]
+
+data Problem = Problem { hole :: [Point]
+                       , figure :: Figure
+                       , epsilon :: Int
+                       } deriving Show
+
+instance FromJSON Problem where
+  parseJSON = withObject "prob" $ \o -> do
+    hole <- o .: "hole"
+    figure <- o .: "figure"
+    epsilon <- o .: "epsilon"
+    return $ Problem hole figure epsilon
+
+instance ToJSON Problem where
+  toJSON (Problem h f e) = object [ "hole" .= h, "figure" .= f, "epsilon" .= e ]
+
 
 -- ## `doctest`のための記述と定義本体
 -- テストは失敗するように書いてある
