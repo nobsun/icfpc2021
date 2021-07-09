a739cdff7486b26442f24afd1d868a3d3548cd1a
Kei Hibino <ex8k.hibino@gmail.com>
2021-07-09 23:10:20 +0900

    add script to get problem JSONs.

---
+++ get-problems.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+(
+    set -e
+
+    set -x
+    cd data/problems
+
+    for i in $(seq 1 59); do
+        n=$(printf "%03d" ${i})
+        if [ -r ${n}.json ]; then
+            continue
+        fi
+        curl --output ${n}.json https://poses.live/problems/${i}/download
+    done
+)
