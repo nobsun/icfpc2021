ad3479a7af4f21ebc95dc842ac468e8e1d0f06d5
cutsea110 <cutsea110@gmail.com>
2021-07-10 12:21:31 +0900

    add -isrc flag

--- test/doctests.hs
+++ test/doctests.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.DocTest
 
 main :: IO ()
-main = doctest [ "-package aeson"
+main = doctest [ "-isrc"
+               , "-package aeson"
                , "src/Parser.hs"
                ]
