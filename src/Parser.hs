237d4464762f89e6909e0c8249174ef4b8c59a51
Kei Hibino <ex8k.hibino@gmail.com>
2021-07-10 18:50:39 +0900

    apply generic programming for array JSONs.

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# OPTIONS -Wno-name-shadowing #-}
 module Parser
     ( Point (..)
@@ -18,10 +20,15 @@ module Parser
     , readProblem
     ) where
 
+import GHC.Generics (Generic)
 -- import Control.Applicative
 import Data.Aeson
 import qualified Data.ByteString.Lazy as LBS
 -- import System.FilePath (FilePath)
+
+import AesonArrayGeneric
+  (genericParseArrayJSON, defaultAesonArrayOptions, genericToArrayJSON)
+
 
 -- | data/problems/001.json をパースして Problem にして印字する
 --   TODO: サンプルなので API を作ったら破棄して OK です
@@ -35,25 +42,36 @@ _test001 = do
 readProblem :: FilePath -> IO (Maybe Problem)
 readProblem fp = decode <$> LBS.readFile fp
 
+-- genericParseArrayJSON_ :: (Generic a, GFromJSON Zero (Rep a), GFromArrayJSON (Rep a))
+--                        => Value -> Parser a
+-- genericParseArrayJSON_ = genericParseArrayJSON defaultAesonArrayOptions defaultOptions
+
+-- genericParseJSON_ :: (Generic a, GFromJSON Zero (Rep a)) => Value -> Parser a
+-- genericParseJSON_ = genericParseJSON defaultOptions
+
 data Point = Point { x :: Int
                    , y :: Int
-                   } deriving (Show, Eq, Ord)
+                   } deriving (Show, Eq, Ord, Generic)
 {- |
 >>> :set -XOverloadedStrings
 >>> decode "[1, 2]" :: Maybe Point
 Just (Point {x = 1, y = 2})
 -}
 instance FromJSON Point where
+  parseJSON = genericParseArrayJSON defaultAesonArrayOptions defaultOptions
+  {-
   parseJSON jsn = do
     [x, y] <- parseJSON jsn
     return $ Point x y
+   -}
 
 {- |
 >>> encode $ Point 1 2
 "[1,2]"
 -}
 instance ToJSON Point where
-  toJSON (Point x y) = toJSON [x, y]
+  toJSON = genericToArrayJSON
+  -- toJSON (Point x y) = toJSON [x, y]
 
 type Index = Int
 
@@ -62,7 +80,7 @@ type Index = Int
 --   NOTE: Index は問題に含まれる vertices の 0 base のインデックスです
 data Edge = Edge { s :: Index -- ^ start
                  , e :: Index -- ^ end
-                 } deriving (Show, Eq)
+                 } deriving (Show, Eq, Generic)
 
 {- |
 >>> :set -XOverloadedStrings
@@ -70,16 +88,20 @@ data Edge = Edge { s :: Index -- ^ start
 Just (Edge {s = 1, e = 2})
 -}
 instance FromJSON Edge where
+  parseJSON = genericParseArrayJSON defaultAesonArrayOptions defaultOptions
+  {-
   parseJSON jsn = do
     [x, y] <- parseJSON jsn
     return $ Edge x y
+   -}
 
 {- |
 >>> encode $ Edge 1 2
 "[1,2]"
 -}
 instance ToJSON Edge where
-  toJSON (Edge x y) = toJSON [x, y]
+  toJSON = genericToArrayJSON
+  -- toJSON (Edge x y) = toJSON [x, y]
 
 type Hole = [Point]
 type Vertices = [Point]
@@ -87,7 +109,7 @@ type Edges = [Edge]
 
 data Figure = Figure { edges    :: Edges
                      , vertices :: Vertices
-                     } deriving (Show, Eq)
+                     } deriving (Show, Eq, Generic)
 
 {- |
 >>> :set -XOverloadedStrings
@@ -95,8 +117,11 @@ data Figure = Figure { edges    :: Edges
 Just (Figure {edges = [Edge {s = 1, e = 2},Edge {s = 3, e = 4}], vertices = [Point {x = 5, y = 6}]})
 -}
 instance FromJSON Figure where
+  parseJSON = genericParseJSON defaultOptions
+  {-
   parseJSON = withObject "figure" $ \o -> do
     Figure <$> o .: "edges" <*> o .: "vertices"
+   -}
 
 {- |
 >>> :set -XDuplicateRecordFields
@@ -106,20 +131,26 @@ instance FromJSON Figure where
 True
 -}
 instance ToJSON Figure where
+  toJSON = genericToJSON defaultOptions
+  {-
   toJSON (Figure es vs)
     = object [ "edges" .= es, "vertices" .= vs]
+   -}
 
 data Problem = Problem { hole    :: Hole
                        , figure  :: Figure
                        , epsilon :: Int
-                       } deriving (Show, Eq)
+                       } deriving (Show, Eq, Generic)
 {- |
 >>> decode "{\"epsilon\":1500,\"hole\":[[10,20],[30,40],[50,60]],\"figure\":{\"edges\":[[1,2],[3,4]],\"vertices\":[[5,6]]}}" :: Maybe Problem
 Just (Problem {hole = [Point {x = 10, y = 20},Point {x = 30, y = 40},Point {x = 50, y = 60}], figure = Figure {edges = [Edge {s = 1, e = 2},Edge {s = 3, e = 4}], vertices = [Point {x = 5, y = 6}]}, epsilon = 1500})
 -}
 instance FromJSON Problem where
+  parseJSON = genericParseJSON defaultOptions
+  {-
   parseJSON = withObject "problem" $ \o -> do
     Problem <$> o .: "hole" <*> o .: "figure" <*> o .: "epsilon"
+   -}
 
 {- |
 >>> :set -XDuplicateRecordFields
@@ -131,10 +162,11 @@ instance FromJSON Problem where
 True
 -}
 instance ToJSON Problem where
-  toJSON (Problem h f e) = object [ "hole" .= h, "figure" .= f, "epsilon" .= e ]
+  toJSON = genericToJSON defaultOptions
+  -- toJSON (Problem h f e) = object [ "hole" .= h, "figure" .= f, "epsilon" .= e ]
 
 
-data Pose = Pose { vertices :: [Point] } deriving (Show, Eq)
+data Pose = Pose { vertices :: [Point] } deriving (Show, Eq, Generic)
 
 {- |
 >>> :set -XOverloadedStrings
@@ -142,12 +174,16 @@ data Pose = Pose { vertices :: [Point] } deriving (Show, Eq)
 Just (Pose {vertices = [Point {x = 1, y = 2},Point {x = 3, y = 4}]})
 -}
 instance FromJSON Pose where
+  parseJSON = genericParseJSON defaultOptions
+  {-
   parseJSON = withObject "pose" $ \o -> do
     Pose <$> o .: "vertices"
+   -}
 
 {- |
 >>> encode $ Pose [Point 1 2, Point 3 4]
 "{\"vertices\":[[1,2],[3,4]]}"
 -}
 instance ToJSON Pose where
-  toJSON (Pose vs) = object [ "vertices" .= vs ]
+  toJSON = genericToJSON defaultOptions
+  -- toJSON (Pose vs) = object [ "vertices" .= vs ]
