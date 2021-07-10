b019aedd3295a127930df0cdbbcff4a6bbf32a0f
cutsea110 <cutsea110@gmail.com>
2021-07-10 16:15:29 +0900

    implement tolerant

--- src/Score.hs
+++ src/Score.hs
@@ -1,6 +1,7 @@
 module Score
   ( sqrDistance
   , dislike
+  , tolerant
   ) where
 
 import Graph
@@ -13,3 +14,15 @@ dislike :: ([GridPoint], [GridPoint]) -> Int
 dislike (hole, pose) = sum [f h | h <- hole] -- map f hole
   where
     f h = minimum [sqrDistance (h, v) | v <- pose]
+
+tolerant :: Int                    -- ^ epsilon
+         -> (GridPoint, GridPoint) -- ^ before
+         -> (GridPoint, GridPoint) -- ^ after
+         -> Bool                   -- ^ result
+tolerant eps (vi, vj) (vi', vj')
+  | d' > d = (10^6 + eps) * d >= 10^6 * d'
+  | d' < d = (10^6 - eps) * d <= 10^6 * d'
+  | otherwise = True
+  where
+    d  = sqrDistance (vi,  vj )
+    d' = sqrDistance (vi', vj')
