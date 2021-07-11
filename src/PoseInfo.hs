ce3fd4b2c1ddfe781b7c8085049926155d2ed68b
Hogeyama <gan13027830@gmail.com>
2021-07-11 11:05:29 +0900

    gui-helper: print dislikes

--- src/PoseInfo.hs
+++ src/PoseInfo.hs
@@ -7,11 +7,10 @@ import Text.Printf (printf)
 
 import qualified Hole
 import qualified Parser                        as P
-                                              hiding (Pose(..))
 import           Parser                         ( Edge(..)
                                                 , Figure(..)
                                                 , Point(..)
-                                                , Pose(Pose)
+                                                , Pose(..)
                                                 , Problem(..)
                                                 )
 import qualified Score
@@ -20,6 +19,7 @@ import qualified TwoDim
 data PoseInfo = PoseInfo
   { poseVertexInfo :: [PoseVertexInfo]
   , poseEdgeInfo   :: [PoseEdgeInfo]
+  , poseDislikes   :: Int
   }
   deriving Show
 
@@ -41,7 +41,7 @@ data PoseEdgeInfo = PoseEdgeInfo
 
 -- TODO rename
 verifyPose :: P.Problem -> Pose -> PoseInfo
-verifyPose Problem { hole, figure, epsilon } (Pose _bonus poseVertices) =
+verifyPose Problem { hole, figure, epsilon } pose@(Pose _bonus poseVertices) =
   let holeEdges = zip hole (tail hole ++ [head hole]) in
   if length (vertices figure) /= length poseVertices then
     error "Wrong number of edge"
@@ -63,10 +63,12 @@ verifyPose Problem { hole, figure, epsilon } (Pose _bonus poseVertices) =
                     , not (any (intersect poseEdge) holeEdges)
                     ]
             in PoseEdgeInfo { .. }
+        poseDislikes = dislike hole pose
       in PoseInfo {..}
 
 reportPose :: PoseInfo -> IO ()
-reportPose PoseInfo{poseEdgeInfo} = do
+reportPose PoseInfo{poseEdgeInfo, poseDislikes} = do
+  putStrLn $ "dislikes: " <> show poseDislikes
   putStrLn "edge     length   possible_range  tolerant included"
   forM_ poseEdgeInfo $ \PoseEdgeInfo{..} -> do
     putStr $ printf "[%02d--%02d] " (fst edgeFromTo) (snd edgeFromTo)
@@ -109,3 +111,10 @@ edgeToTuple (s, e) = (pointToTuple s, pointToTuple e)
 
 sqrDistance :: (Point, Point) -> Int
 sqrDistance (Point x1 y1, Point x2 y2) = Score.sqrDistance ((x1, y1), (x2, y2))
+
+dislike :: P.Hole -> P.Pose -> Int
+dislike hole Pose{pose'vertices}= Score.dislike (hole',pose')
+  where
+    hole' = map pointToTuple hole
+    pose' = map pointToTuple pose'vertices
+
